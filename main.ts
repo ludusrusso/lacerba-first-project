1e7c7c0d4c9f5a010fa9c5573e7b106af468a4b7
Ludovico Russo <l.russo@hoplo.com>
2020-08-12 14:43:41 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+console.log('Hello World!');
+console.log('🚀 This is my first Node Project!');
+
+let myVar: number = 3;
+myVar += 10; // 13
+
+console.log('myVar = ', myVar);
+
+interface MyInterface {
+  myNumber: number;
+  myString: string;
+}
+
+let myNewVariable: MyInterface = {
+  myNumber: 10,
+  myString: 'ciao',
+};
+
+console.log('myNewVariable = ', myNewVariable);
